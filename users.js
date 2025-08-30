cd543ea6aa4c513f55928a00f90f9d5e9a7d87de
anaslila <lilaanas6@gmail.com>
2025-08-30 18:58:48 +0530

    Update users.js

--- users.js
+++ users.js
@@ -7,7 +7,7 @@ const users = [
     { 
         username: "anas", 
         password: "anas123", 
-        profilePic: "" 
+        profilePic: "https://media.licdn.com/dms/image/v2/D4D03AQHdREF9JrFrDA/profile-displayphoto-shrink_400_400/B4DZbyq1D5GYAo-/0/1747828036502?e=1759363200&v=beta&t=hBttcPAwNvchZzGpNgnKkqH1Gdgb0pjbH-VqWPFgGwM" 
     },
     {
         username: "john",
@@ -20,3 +20,4 @@ const users = [
         profilePic: "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=150&h=150&fit=crop&crop=face"
     }
 ];
+
